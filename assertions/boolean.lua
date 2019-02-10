5e18911f18dfd145734e05928526d0550b932340
Ryan <rsaridar@outlook.com>
2019-02-10 01:13:36 +0000

    Added isTrue and isFalse assertions for booleans
    The isTrue assertion works like the built-in assertion function, in that an exception is thrown if the value passed is false. This can be used to make assertions with custom conditions more easily (without having to define a new assertion item).

--- assertions/boolean.lua
+++ assertions/boolean.lua
@@ -4,4 +4,12 @@ local booleanAssertion = flow.assertionFactory.new(function(data)
 	return dataType == "boolean", "TYPE_RESTRICTION_VIOLATION", "Boolean expected, got " .. dataType;
 end);
 
+booleanAssertion.addSubAssertion("isTrue", function(data)
+	return data == true, "VALUE_MISMATCH", "Expected true";
+end);
+
+booleanAssertion.addSubAssertion("isFalse", function(data)
+	return data == false, "VALUE_MISMATCH", "Expected false";
+end);
+
 return booleanAssertion;
